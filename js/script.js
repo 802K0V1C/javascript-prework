4e22ee28f3449b5b639207b38a24d62de8a80e54
Artur Bożek <arturhubert.bozek@gmail.com>
2022-12-13 17:59:17 +0100

    Add player moves

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Computer moves
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -5,13 +6,31 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
-  computerMove = 'kamień';
+  	computerMove = 'kamień';
 }
 else if(randomNumber == 2){
-	computerMove = 'nozyce';
+	computerMove = 'nożyce';
 }
 else if(randomNumber == 3){
 	computerMove = 'papier';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
+
+//Player moves
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  	playerMove = 'kamień';
+}
+else if (playerInput =='2'){
+	playerMove = 'nożyce';
+}
+else if (playerInput =='3'){
+	playerMove = 'papier';
+}
+printMessage('Twój ruch to: ' + playerMove);
